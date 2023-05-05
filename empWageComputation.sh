327555826ca7c5e92ea324d87a030492aef2099f
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-05 16:59:12 +0530

    Add:  employee Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,14 @@
 echo "welcome EmployeeWageComputationProblem where we are calculating the salary of the employee"
+isPresent=1;
+randomcheck=$((RANDOM%2));
+if [ $isPresent -eq $randomcheck ]
+then
+	echo "Employee is present"
+empRateperHr=20
+empHrs=8
+salary=$(($empHrs*$empRateperHr))
+echo "salary="$salary
+else
+echo "Employee is absent"
+echo "salary=0"
+fi
