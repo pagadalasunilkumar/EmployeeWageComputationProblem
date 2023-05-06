df416cd3fb35f77c7b7f1d26146cfbf966edd072
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-06 10:01:08 +0530

    Add: Day andthe Daily Wagealong with the TotalWage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,7 +2,7 @@ echo "Welcome to EmployeeWageComputationProblem where we are calculating the sal
 function getWorkingHours() {
     case $1 in
         1)
-            echo 8 ;; 
+            echo 8 ;;  
         2)
             echo 4 ;;  
         *)
@@ -13,7 +13,7 @@ IS_FULL_TIME=1
 IS_PART_TIME=2
 EMP_RATE_PER_HR=20
 MAX_WORKING_DAYS=20
-MAX_WORKING_HRS=100
+MAX_WORKING_HRS=10
 totalWorkingDays=0
 totalWorkingHrs=0
 totalSalary=0
@@ -29,7 +29,7 @@ do
     totalSalary=$((totalSalary + salary))
 done
 
-echo "Daily Wages:"
+echo "Day   Wage"
 for ((day=1; day<=totalWorkingDays; day++))
 do
     echo "Day $day: ${dailyWage[$day]}"
